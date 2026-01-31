589268182ec100cd94ef314354b0ca76e8717a17
uttam tiwari <118200430+87690ut@users.noreply.github.com>
2026-01-31 15:15:26 +0530

    Time: 286 ms (48.74%), Space: 67 MB (70.38%) - LeetHub

--- 2880-select-data/2880-select-data.py
+++ 2880-select-data/2880-select-data.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    anw = students.loc[students['student_id'] == 101,['name','age']]
-    return anw
+    return students.loc[students['student_id'] == 101,['name','age']]
+    
