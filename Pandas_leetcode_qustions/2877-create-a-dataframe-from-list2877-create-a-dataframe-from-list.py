29dff6ada31e6b33c7d4d5dc5c1f946126db4bbb
uttam tiwari <118200430+87690ut@users.noreply.github.com>
2026-02-05 18:40:36 +0530

    Delete Pandas_leetcode_qustions/2877-create-a-dataframe-from-list2877-create-a-dataframe-from-list.py

--- Pandas_leetcode_qustions/2877-create-a-dataframe-from-list2877-create-a-dataframe-from-list.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    col = ['student_id','age']
-    ans = pd.DataFrame(student_data,columns=col)
-    return ans
